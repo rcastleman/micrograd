9a4d4dcc92324f2988051ea1a633b8e90c88f8c0
Randy Castleman <rcastleman@gmail.com>
2024-03-18 22:32:56 -0400

    plotting tanh

--- instruction.py
+++ instruction.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+plt.plot(np.tanh(np.arange(-5,5,0.2)));plt.grid()
 plt.plot(np.arange(-5,5,0.2)),np.tanh(np.arange(-5,5,0.2));plt.grid()
 
 
