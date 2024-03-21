50f304102041c41b489761729b0e56b2bb85e43b
Randy Castleman <rcastleman@gmail.com>
2024-03-20 21:57:59 -0400

    no visual calc of 'n'

--- instruction.py
+++ instruction.py
@@ -186,11 +186,14 @@ x2 = Value(0.0, label='x2')
 w1 = Value(-3.0, label='w1')
 w2 = Value(1.0, label='w2')
 # bias of the neuron
-b = Value(6.8813735870195432, label='b')
+b = Value(6.7, label='b')
 # x1*w1 + x2*w2 + b
 x1w1 = x1*w1; x1w1.label = 'x1*w1'
 x2w2 = x2*w2; x2w2.label = 'x2*w2'
 x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = 'x1*w1 + x2*w2'
 n = x1w1x2w2 + b; n.label = 'n'
-o = n.tanh(); o.label = 'o'
+# o = n.tanh(); o.label = 'o'
 
+print(f"x1 = {x1.data}, x2 = {x2.data}, w1 = {w1.data}, w2 ={w2.data}")
+print(f"x1 * w1 = {x1w1.data}, x2 * w2 = {x2w2.data} and x1w1 + x2w2 = {x1w1x2w2.data}")
+print(f"and b = {b.data} so n = {n.data}")
