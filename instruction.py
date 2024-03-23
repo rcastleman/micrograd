01e8e5a60ded57dd6f20cb1ac4e4888c92fde956
Randy Castleman <rcastleman@gmail.com>
2024-03-22 21:38:09 -0400

    b = 6.8813735870195432

--- instruction.py
+++ instruction.py
@@ -186,7 +186,7 @@ x2 = Value(0.0, label='x2')
 w1 = Value(-3.0, label='w1')
 w2 = Value(1.0, label='w2')
 # bias of the neuron
-b = Value(80.0, label='b')
+b = Value(6.8813735870195432, label='b')
 # x1*w1 + x2*w2 + b
 x1w1 = x1*w1; x1w1.label = 'x1*w1'
 x2w2 = x2*w2; x2w2.label = 'x2*w2'
