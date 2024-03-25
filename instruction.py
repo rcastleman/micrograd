ac3d2a9501e8a65e7b5b3bc0316787c0b5b7b0d8
Randy Castleman <rcastleman@gmail.com>
2024-03-24 21:36:54 -0400

    fixed tanh

--- instruction.py
+++ instruction.py
@@ -104,21 +104,27 @@ class Value:
         def _backward():
            self.grad = 1.0 * out.grad
            other.grad = 1.0 * out.grad
-        
         out._backward = _backward
         return out
     
     def __mul__(self,other):    
         out = Value(self.data * other.data,(self,other),'*')
+        def _backward():
+           self.grad = other.data * out.grad
+           other.grad = self.data * out.grad
+        out._backward = _backward
         return out
     
     def tanh(self):
-       x = self.data
-       t = (math.exp(2*x) - 1)/(math.exp(2*x)+1)
-       out = Value(t, (self,),'tanh')
-       return out
-
+      x = self.data
+      t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
+      out = Value(t, (self, ), 'tanh')
+    
+      def _backward():
+        self.grad += (1 - t**2) * out.grad
+      out._backward = _backward
        
+      return out
 
 # a = Value(2.0, label = 'a')
 # b = Value(-3.0, label = 'b')
