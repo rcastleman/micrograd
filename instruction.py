3827286790cb3d5b21cba664019d66e86d85b9a8
Randy Castleman <rcastleman@gmail.com>
2024-03-24 21:18:27 -0400

    added _backward

--- instruction.py
+++ instruction.py
@@ -91,6 +91,7 @@ class Value:
     def __init__(self,data,_children=(),_op='',label = ''):
         self.data = data
         self.grad = 0.0
+        self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op
         self.label = label
@@ -100,6 +101,11 @@ class Value:
     
     def __add__(self,other):
         out = Value(self.data + other.data,(self,other),'+')
+        def _backward():
+           self.grad = 1.0 * out.grad
+           other.grad = 1.0 * out.grad
+        
+        out._backward = _backward
         return out
     
     def __mul__(self,other):    
