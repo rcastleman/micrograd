5e47d5bae3d3fc5ca089370e5da379aec73444e2
Randy Castleman <rcastleman@gmail.com>
2024-04-07 19:42:50 -0400

    uncommented

--- instruction.py
+++ instruction.py
@@ -100,7 +100,7 @@ class Value:
         return f"Value(data={self.data})"
     
     def __add__(self,other):
-        # other = other if isinstance(other, Value) else Value(other)
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data,(self,other),'+')
         def _backward():
            self.grad += 1.0 * out.grad
