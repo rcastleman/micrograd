e7093e8bae54133d0713fd81a67fa8c833750e63
Randy Castleman <rcastleman@gmail.com>
2024-04-09 22:23:26 -0400

    __rmul__

--- instruction.py
+++ instruction.py
@@ -279,5 +279,5 @@ plt.plot(np.arange(-5,5,0.2), np.tanh(np.arange(-5,5,0.2))); plt.grid()
 # print(draw_dot(x1w1x2w2))
 
 a = Value(2.0)
-b = a * 3.0
+b = 2 * a
 print(f"b = {b} where a.data = {a.data}")
