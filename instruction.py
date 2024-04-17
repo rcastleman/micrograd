42075771d355bee49b015597dd662fa317ca68fe
Randy Castleman <rcastleman@gmail.com>
2024-04-16 22:51:19 -0400

    added Torch

--- instruction.py
+++ instruction.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import torch
 plt.plot(np.tanh(np.arange(-5,5,0.2)));plt.grid()
 plt.plot(np.arange(-5,5,0.2)),np.tanh(np.arange(-5,5,0.2));plt.grid()
 
@@ -323,3 +324,20 @@ b = Value(4.0)
 # print(f"b = {b.data}")
 print(f"a - b = {a-b}")
 # print(f"b = {b} where a.data = {a.data}")
+
+x1 = torch.Tensor([2.0]).double()                ; x1.requires_grad = True
+x2 = torch.Tensor([0.0]).double()                ; x2.requires_grad = True
+w1 = torch.Tensor([-3.0]).double()               ; w1.requires_grad = True
+w2 = torch.Tensor([1.0]).double()                ; w2.requires_grad = True
+b = torch.Tensor([6.8813735870195432]).double()  ; b.requires_grad = True
+n = x1*w1 + x2*w2 + b
+o = torch.tanh(n)
+
+print(o.data.item())
+o.backward()
+
+print('---')
+print('x2', x2.grad.item())
+print('w2', w2.grad.item())
+print('x1', x1.grad.item())
+print('w1', w1.grad.item())
