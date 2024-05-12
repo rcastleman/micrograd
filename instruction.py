d4a48811254222ccd0a4642729b354d96c496732
Randy Castleman <rcastleman@gmail.com>
2024-05-12 11:39:47 -0400

    fixed parens

--- instruction.py
+++ instruction.py
@@ -373,7 +373,7 @@ class Layer:
    
    def __call__(self,x):
        outs = [n(x)for n in self.neurons]
-       return outs[0] if len(outs==1) else outs
+       return outs[0] if len(outs)==1 else outs
    
    def __init__(self,nin,nout):
       self.neurons = [Neuron(nin) for _ in range(nout)]
